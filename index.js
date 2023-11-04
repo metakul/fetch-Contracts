9fa772ce7ee21081612ded77709672f0b43390cf
Shubhamkunwar10 <kshubham@c3ihub.ittk.ac.in>
2023-11-04 19:02:43 +0530

    updates

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ app.use(express.json());
 
 app.get("/fetchAllNFTs", async (req, res) => {
   try {
-    const sdk = new ThirdwebSDK("polygon", {
+    const sdk = new ThirdwebSDK("mumbai", {
       secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
     });
 
-    const contract = await sdk.getContract("0x710E9161e8A768c0605335AB632361839f761374");
+    const contract = await sdk.getContract("0x6b2faBa274A09F6b64fd57b4367ee09aF7793A74");
 
     const nfts = await contract.erc721.getAll();
     res.json(nfts);
@@ -30,14 +30,32 @@ app.get("/fetchAllNFTs", async (req, res) => {
     res.status(500).json({ error: "An error occurred while fetching NFTs." });
   }
 });
-app.post("/fetchTokenBalance", async (req, res) => {
+app.get("/fetchTokenBalance/:walletAddress", async (req, res) => {
   try {
-    const walletAddress = req.body.walletAddress;
-    const sdk = new ThirdwebSDK("polygon", {
+    const walletAddress = req.params.walletAddress;
+
+    const sdk = new ThirdwebSDK("mumbai", {
       secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
     });
 
-    const contract = await sdk.getContract("0xE9fd323D7B1e4cFd07C657E218F7da16efd6532f");
+    const contract = await sdk.getContract("0xD650834b4A40C58482F68320bC248eDe98D6506F");
+
+    const balance = await contract.erc20.balanceOf(walletAddress);
+    res.json(balance);
+  } catch (error) {
+    console.error("Error fetching NFTs:", error);
+    res.status(500).json({ error: "An error occurred while fetching NFTs." });
+  }
+});
+app.get("/fetchTokenBalance/:walletAddress", async (req, res) => {
+  try {
+    const walletAddress = req.params.walletAddress;
+
+    const sdk = new ThirdwebSDK("mumbai", {
+      secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
+    });
+
+    const contract = await sdk.getContract("0xD650834b4A40C58482F68320bC248eDe98D6506F");
 
     const balance = await contract.erc20.balanceOf(walletAddress);
     res.json(balance);
@@ -50,19 +68,26 @@ app.post("/fetchTokenBalance", async (req, res) => {
 app.get("/getNFTsForWallet/:walletAddress", async (req, res) => {
   try {
     const walletAddress = req.params.walletAddress;
+
     const sdk = new ThirdwebSDK("mumbai", {
       secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
     });
 
-    const contract = await sdk.getContract("0xD97f7972A9f82f775d6a528f04Ff937EaDFFBCE7");
+    const contract = await sdk.getContract("0x6b2faBa274A09F6b64fd57b4367ee09aF7793A74");
 
-    const balance = await contract.erc721.balanceOf(walletAddress);
-    res.json({ balance });
+    // Load all NFTs
+    const allNFTs = await contract.erc721.getAll();
+
+    // Get the NFTs owned by the specified wallet address
+    const nftsOwnedByWallet = allNFTs.filter((nft) => nft.owner.toLowerCase() === walletAddress.toLowerCase());
+
+    res.json(nftsOwnedByWallet);
   } catch (error) {
-    console.error("Error fetching NFT balance for wallet:", error);
-    res.status(500).json({ error: "An error occurred while fetching NFT balance for the wallet." });
+    console.error("Error fetching NFTs for wallet:", error);
+    res.status(500).json({ error: error });
   }
 });
+
 
 // Route to fetch an NFT using a tokenId from req.body
 app.post("/getNFTByTokenId", async (req, res) => {
@@ -81,22 +106,7 @@ app.post("/getNFTByTokenId", async (req, res) => {
     res.status(500).json({ error: "An error occurred while fetching NFT by tokenId." });
   }
 });
-app.post("/claimNft", async (req, res) => {
-  try {
-    const sdk = new ThirdwebSDK("polygon", {
-      secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
-    });
 
-    const contract = await sdk.getContract("0x710E9161e8A768c0605335AB632361839f761374");
-
-    const quantity = 1;
-    const tx = await contract.erc721.claim.prepare(quantity);
-        res.json(tx);
-  } catch (error) {
-    console.error("Error fetching NFT by tokenId:", error);
-    res.status(500).json({ error });
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
