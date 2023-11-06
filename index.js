27accc806a5bc2bdda56707c70f1cd229fc2bdeb
Shubhamkunwar10 <kshubham@c3ihub.ittk.ac.in>
2023-11-06 15:47:45 +0530

    total rewards

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { ThirdwebSDK } = require("@thirdweb-dev/sdk");
-const cors = require('cors');
+const cors = require("cors");
+const Web3Utils = require("web3-utils");
 
 const app = express();
 const port = 8004; // You can choose a different port if needed
 
 // Middleware to parse JSON request bodies
 const corsOptions = {
-  origin: '*', // Allow requests from all origins
+  origin: "*", // Allow requests from all origins
 };
-
 
 app.use(cors(corsOptions)); // Use the cors middleware with the specified options
 
@@ -18,10 +18,13 @@ app.use(express.json());
 app.get("/fetchAllNFTs", async (req, res) => {
   try {
     const sdk = new ThirdwebSDK("mumbai", {
-      secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
+      secretKey:
+        "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
     });
 
-    const contract = await sdk.getContract("0x6b2faBa274A09F6b64fd57b4367ee09aF7793A74");
+    const contract = await sdk.getContract(
+      "0x6b2faBa274A09F6b64fd57b4367ee09aF7793A74"
+    );
 
     const nfts = await contract.erc721.getAll();
     res.json(nfts);
@@ -35,10 +38,13 @@ app.get("/fetchTokenBalance/:walletAddress", async (req, res) => {
     const walletAddress = req.params.walletAddress;
 
     const sdk = new ThirdwebSDK("mumbai", {
-      secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
+      secretKey:
+        "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
     });
 
-    const contract = await sdk.getContract("0xD650834b4A40C58482F68320bC248eDe98D6506F");
+    const contract = await sdk.getContract(
+      "0xD650834b4A40C58482F68320bC248eDe98D6506F"
+    );
 
     const balance = await contract.erc20.balanceOf(walletAddress);
     res.json(balance);
@@ -47,15 +53,18 @@ app.get("/fetchTokenBalance/:walletAddress", async (req, res) => {
     res.status(500).json({ error: "An error occurred while fetching NFTs." });
   }
 });
-app.get("/fetchTokenBalance/:walletAddress", async (req, res) => {
+app.get("/fetchERC20TokenBalance/:walletAddress", async (req, res) => {
   try {
     const walletAddress = req.params.walletAddress;
 
     const sdk = new ThirdwebSDK("mumbai", {
-      secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
+      secretKey:
+        "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
     });
 
-    const contract = await sdk.getContract("0xD650834b4A40C58482F68320bC248eDe98D6506F");
+    const contract = await sdk.getContract(
+      "0xD650834b4A40C58482F68320bC248eDe98D6506F"
+    );
 
     const balance = await contract.erc20.balanceOf(walletAddress);
     res.json(balance);
@@ -70,16 +79,20 @@ app.get("/getNFTsForWallet/:walletAddress", async (req, res) => {
     const walletAddress = req.params.walletAddress;
 
     const sdk = new ThirdwebSDK("mumbai", {
-      secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
+      secretKey:
+        "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
     });
 
-    const contract = await sdk.getContract("0x6b2faBa274A09F6b64fd57b4367ee09aF7793A74");
-
+    const contract = await sdk.getContract(
+      "0x6b2faBa274A09F6b64fd57b4367ee09aF7793A74"
+    );
     // Load all NFTs
     const allNFTs = await contract.erc721.getAll();
 
     // Get the NFTs owned by the specified wallet address
-    const nftsOwnedByWallet = allNFTs.filter((nft) => nft.owner.toLowerCase() === walletAddress.toLowerCase());
+    const nftsOwnedByWallet = allNFTs.filter(
+      (nft) => nft.owner.toLowerCase() === walletAddress.toLowerCase()
+    );
 
     res.json(nftsOwnedByWallet);
   } catch (error) {
@@ -87,26 +100,83 @@ app.get("/getNFTsForWallet/:walletAddress", async (req, res) => {
     res.status(500).json({ error: error });
   }
 });
+app.get("/getstakedNFTsForWallet/:walletAddress", async (req, res) => {
+  try {
+    const walletAddress = req.params.walletAddress;
+    console.log(walletAddress)
 
+    const sdk = new ThirdwebSDK("mumbai", {
+      secretKey:
+        "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
+    });
+
+    const contract = await sdk.getContract(
+      "0x7d19bFc22fcF16bCDF4D89607a8D79994AD11f47"
+    );
+    const nftContract = await sdk.getContract(
+      "0x6b2faBa274A09F6b64fd57b4367ee09aF7793A74"
+    );
+
+    const nftsOwnedByWallet = await contract.call("getStakeInfo", [
+      walletAddress,
+    ]);
+
+    // Extract the token IDs from the response
+    const tokenIds = nftsOwnedByWallet[0];
+    console.log(tokenIds);
+
+// Fetch the total reward (assuming it's constant)
+const totalRewardHex = nftsOwnedByWallet[1]._hex;
+const totalRewardWei = BigInt(totalRewardHex);
+const totalReward = Number(totalRewardWei / BigInt(1e18)); // Convert to a regular number
+console.log(totalReward); // This will be the total reward as a normal number
+
+    // Create an array to store information for each tokenId and details
+    const nftInfo = [];
+
+    for (const tokenId of tokenIds) {
+      // Fetch information for the current tokenId
+      const nftDetails = await nftContract.erc721.get(tokenId);
+
+      // Push the details into the nftInfo array
+      nftInfo.push(nftDetails);
+    }
+
+    // Create the response object with nftDetails and totalReward
+    const responseObject = {
+      nftDetails: nftInfo,
+      totalReward: totalReward,
+    };
+
+    res.json(responseObject);
+  } catch (error) {
+    console.error("Error fetching NFTs for wallet:", error);
+    res.status(500).json({ error: error });
+  }
+});
 
 // Route to fetch an NFT using a tokenId from req.body
 app.post("/getNFTByTokenId", async (req, res) => {
   try {
     const tokenId = req.body.tokenId; // Assuming the request body contains a tokenId field
     const sdk = new ThirdwebSDK("polygon", {
-      secretKey: "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
+      secretKey:
+        "U44uvKQJkQYLdnDdxHfBnjQDfG_mx6jUUulCvo2l9UyJGvqx2inzPu2EypIvVrnonMtAW_2h2Dn0Z3Rfux2LHg",
     });
 
-    const contract = await sdk.getContract("0x710E9161e8A768c0605335AB632361839f761374");
+    const contract = await sdk.getContract(
+      "0x710E9161e8A768c0605335AB632361839f761374"
+    );
 
     const nft = await contract.erc721.get(tokenId);
     res.json(nft);
   } catch (error) {
     console.error("Error fetching NFT by tokenId:", error);
-    res.status(500).json({ error: "An error occurred while fetching NFT by tokenId." });
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching NFT by tokenId." });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
